40f9235223ddd9b576adb6952087e8dfa03bde98
二也 <1194445212@qq.com>
2019-07-08 18:39:16 +0800

    1.更新eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 // .eslintrc
+// 说明：该项目中不检查代码风格，只做静态语法检查。
 module.exports = {
   env: {
     commonjs: true,
     es6: true,
     node: true,
   },
-  extends: 'eslint:recommended',
+  extends: [],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -14,9 +15,29 @@ module.exports = {
     ecmaVersion: 2018,
   },
   rules: {
-    indent: ['error', 2],
-    'linebreak-style': ['error', 'windows'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
+    // Possible Errors
+    // https://cn.eslint.org/docs/rules/#possible-errors
+    'no-console': 'error',
+
+    // Best Practices
+    // https://cn.eslint.org/docs/rules/#best-practices
+    'class-methods-use-this': 'error',
+
+    // Variables
+    // https://cn.eslint.org/docs/rules/#variables
+    'no-undef': 'error',
+    'no-unused-vars': 'error',
+
+    // Node.js and CommonJS
+    // https://cn.eslint.org/docs/rules/#nodejs-and-commonjs
+
+    // ECMAScript 6
+    // https://cn.eslint.org/docs/rules/#ecmascript-6
+    'callback-return': 'error',
+    'global-require': 'error',
+    'handle-callback-err': 'error',
+    'no-buffer-constructor': 'error',
+    'no-process-env': 'error',
+    'no-process-exit': 'error',
   },
 };
