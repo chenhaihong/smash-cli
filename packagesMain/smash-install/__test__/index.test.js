26549f1d292835909b9c32f1e0c166a1cd8ca1c9
二也 <1194445212@qq.com>
2019-08-06 17:58:07 +0800

    1.更新smash-install测试用例

--- packagesMain/smash-install/__test__/index.test.js
+++ packagesMain/smash-install/__test__/index.test.js
@@ -8,7 +8,8 @@ const TEMP = resolve(lastCwd, 'temp');
 
 const spySuccess = jest.spyOn(logger, 'success');
 const spyFail = jest.spyOn(logger, 'fail');
-const spyInstall = jest.fn(smashInstall);
+const spyCopySync = jest.spyOn(fse, 'copySync');
+const mockInstall = jest.fn(smashInstall);
 
 beforeAll(() => {
   fse.emptyDirSync(TEMP);
@@ -24,37 +25,29 @@ afterAll(() => {
   fse.removeSync(TEMP); // 重置工作空间后，才能接触对temp目录的占用，然后可删除生成的配置文件
 });
 
-// {
-//   // 修改工作目录到./temp目录
-//   const wd = path.resolve(__dirname, './temp');
-//   fse.ensureDirSync(wd);
-//   process.chdir(wd);
-
-//   // 测试安装模板方法
-//   smashInstall('smash-middleware-helloworld');
-// }
-
 describe('smash-install', () => {
   test('should install well', async () => {
-    expect.assertions(3);
+    expect.assertions(4);
 
-    const tplName = 'smash-middleware-helloworld';
-    await smashInstall(tplName);
+    const tplName = 'smash-template-react';
+    await mockInstall(tplName);
 
-    expect(spyInstall).toBeCalled();
-    expect(spyInstall.mock.calls[0][0]).toBe(tplName);
+    expect(mockInstall).toBeCalled();
+    expect(mockInstall.mock.calls[0][0]).toBe(tplName);
+
+    expect(spyCopySync).toBeCalled();
 
     expect(spySuccess).toBeCalled();
   });
 
-  test('should not install well', () => {
+  test('should not install well', async () => {
     expect.assertions(3);
 
     const tplName = 'smsah-waremiddle-helloworld';
-    await smashInstall(tplName);
+    await mockInstall(tplName);
 
-    expect(spyInstall).toBeCalled();
-    expect(spyInstall.mock.calls[0][0]).toBe(tplName);
+    expect(mockInstall).toBeCalled();
+    expect(mockInstall.mock.calls[0][0]).toBe(tplName);
 
     expect(spyFail).toBeCalled();
   });
