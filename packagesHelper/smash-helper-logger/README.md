3689d3fc52a65f54581912e49c3b3c6a1f156b26
二也 <1194445212@qq.com>
2019-08-06 12:24:11 +0800

    更新mash-helper-logger
     1.添加jsdoc；
     2.更新readme

--- packagesHelper/smash-helper-logger/README.md
+++ packagesHelper/smash-helper-logger/README.md
@@ -7,15 +7,10 @@ Logger helper for `smash-cli`.
 ```javascript
 const logger = require('smash-helper-logger');
 
-logger.log('Hello world.');
-// [smash] [log]   hello world.
-
-logger.info('Hello world.');
-// [smash] [info]  hello world.
-
-logger.warn('Hello world.');
-// [smash] [warn]  hello world.
-
-logger.error('Hello world.');
-// [smash] [error] hello world.
+logger.log('Hello world.'); // smash log hello world.
+logger.info('Hello world.'); // smash info hello world.
+logger.warn('Hello world.'); // smash warn hello world.
+logger.error('Hello world.'); // smash error hello world.
+logger.success('Hello world.'); // smash success hello world.
+logger.fail('Hello world.'); // smash fail hello world.
 ```
--- packagesHelper/smash-helper-logger/lib/index.js
+++ packagesHelper/smash-helper-logger/lib/index.js
@@ -14,43 +14,55 @@ module.exports = {
 };
 
 /**
- *
+ * 输出普通提示
+ * @param  {...any} params 要输出的信息
+ * @returns {void}
  */
-function log() {
-  console.log(_name, chalk.blueBright('log'), ...arguments); // eslint-disable-line
+function log(...params) {
+  console.log(_name, chalk.blueBright('log'), ...params); // eslint-disable-line
 }
 
 /**
- *
+ * 输出信息
+ * @param  {...any} params 要输出的信息
+ * @returns {void}
  */
-function info() {
-  console.log(_name, chalk.greenBright('info'), ...arguments); // eslint-disable-line
+function info(...params) {
+  console.log(_name, chalk.greenBright('info'), ...params); // eslint-disable-line
 }
 
 /**
- *
+ * 输出警告
+ * @param  {...any} params 要输出的信息
+ * @returns {void}
  */
-function warn() {
-  console.log(_name, chalk.yellowBright('warn'), ...arguments); // eslint-disable-line
+function warn(...params) {
+  console.log(_name, chalk.yellowBright('warn'), ...params); // eslint-disable-line
 }
 
 /**
- *
+ * 输出错误
+ * @param  {...any} params 要输出的信息
+ * @returns {void}
  */
-function error() {
-  console.log(_name, chalk.redBright('error'), ...arguments); // eslint-disable-line
+function error(...params) {
+  console.log(_name, chalk.redBright('error'), ...params); // eslint-disable-line
 }
 
 /**
- *
+ * 输出成功提示
+ * @param  {...any} params 要输出的信息
+ * @returns {void}
  */
-function success() {
-  console.log(_name, chalk.greenBright('success'), ...arguments); // eslint-disable-line
+function success(...params) {
+  console.log(_name, chalk.greenBright('success'), ...params); // eslint-disable-line
 }
 
 /**
- *
+ * 输出失败提示
+ * @param  {...any} params 要输出的信息
+ * @returns {void}
  */
-function fail() {
-  console.log(_name, chalk.redBright('fail'), ...arguments); // eslint-disable-line
+function fail(...params) {
+  console.log(_name, chalk.redBright('fail'), ...params); // eslint-disable-line
 }
