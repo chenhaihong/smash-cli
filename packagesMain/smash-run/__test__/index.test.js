8eb68df637f8fd7859bd8ec617ea25db91f0a88b
二也 <1194445212@qq.com>
2019-08-07 09:59:51 +0800

    1.更新smash-run测试用例

--- packagesMain/smash-run/__test__/index.test.js
+++ packagesMain/smash-run/__test__/index.test.js
@@ -28,7 +28,7 @@ const spyExtract = jest.spyOn(pacote, 'extract');
 // spy on QuereRunner
 const spyDequeue = jest.spyOn(QueueRunner, 'dequeue');
 // mock smash-run
-const mockRun = jest.fn(smashRun);
+const mockSmashRun = jest.fn(smashRun);
 
 beforeAll(() => {
   fse.emptyDirSync(REPO); // 清空中间件仓库
@@ -48,43 +48,56 @@ afterAll(() => {
 });
 
 describe('smash-run', () => {
-  it('should run known task well', async () => {
-    await mockRun('helloworld');
+  it('should run known task and install first-time-run middleware', async () => {
+    await mockSmashRun('helloworld');
 
-    expect(mockRun).toBeCalled();
-
+    // should run known task well
+    expect(mockSmashRun).toBeCalled();
     expect(spyGetMiddlewareQueue).toBeCalled();
-    expect(spyGetInstalledPaths).toBeCalled();
+    expect(spyDequeue).toBeCalled();
 
+    // should install first-time-run middleware
     // 首次使用中间件需要安装
+    expect(spyGetInstalledPaths).toBeCalled();
     expect(spyExtract).toBeCalled();
     expect(spySuccess).toBeCalled();
-    expect(spyDequeue).toBeCalled();
   });
 
-  it('should not run unknown task', async () => {
-    await mockRun('worldhello');
+  it('should not run unknown task well', async () => {
+    await mockSmashRun('worldhello');
 
+    expect(mockSmashRun).toBeCalled();
+    expect(spyGetMiddlewareQueue).toBeCalled();
+
+    // 不会运行未知的task
+    expect(spyGetInstalledPaths).not.toBeCalled();
+    expect(spyDequeue).not.toBeCalled();
+
+    // 直接进入错误分支，输出错误信息
     expect(spyWarn).toBeCalled();
     expect(spyWarn.mock.calls[0]).toContain('task not found.');
   });
 
-  it('should not re-install smash-middleware-helloworld middleware', async () => {
-    await mockRun('helloworld');
+  it('should not re-install not-first-time-run middleware', async () => {
+    await mockSmashRun('helloworld');
+
+    expect(mockSmashRun).toBeCalled();
+    expect(spyGetMiddlewareQueue).toBeCalled();
 
     // 再次使用中间件不需要安装
+    expect(spyGetInstalledPaths).toBeCalled();
     expect(spyExtract).not.toBeCalled();
   });
 
-  it('should not run task well with unknown middleware', async () => {
-    await mockRun('task-with-unknown-middleware');
+  it('should not run known task with unknown middleware sucessfully', async () => {
+    await mockSmashRun('task-with-unknown-middleware'); // 运行未知task
 
     expect(spyGetMiddlewareQueue).toBeCalled();
-    expect(spyGetInstalledPaths).toBeCalled(); // 执行到这里后，代码必须报错，才是正常的结果
+    expect(spyGetInstalledPaths).toBeCalled(); // 代码执行到这里后，代码必须报错，才是正常的结果
 
-    expect(spyExtract).not.toBeCalled();
-    expect(spyDequeue).not.toBeCalled();
+    expect(spyExtract).not.toBeCalled(); // 因为不存在中间件，所以不会进入下载中间件的逻辑
+    expect(spyDequeue).not.toBeCalled(); // 不会执行中间件队列任务
 
-    expect(spyError).toBeCalled();
+    expect(spyError).toBeCalled(); // 捕获异常时，通过logger.error输出了错误信息
   });
 });
