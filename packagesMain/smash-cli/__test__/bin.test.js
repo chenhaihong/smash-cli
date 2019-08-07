fd5dd3a45da3397085abc4c7a677088fcad17cd2
erye <erye@tiiit.cn>
2019-08-07 21:53:57 +0800

    1.更新测试用例

--- packagesMain/smash-cli/__test__/bin.test.js
+++ packagesMain/smash-cli/__test__/bin.test.js
@@ -12,58 +12,84 @@ const spySmashInstall = jest.spyOn(programmaticSmash, 'install');
 const spySmashRun = jest.spyOn(programmaticSmash, 'run');
 
 const ROOT = resolve(__dirname, '..'); // 该包的根目录
-const TEMP_CWD = resolve(ROOT, '__temp__');
+const TEMP = resolve(ROOT, '__temp__');
 const smashBin = resolve(ROOT, 'lib/bin.js');
 const bin = (cwd) => (command) => execShPromise(`node ${smashBin} ${command}`, { cwd, stdio: null });
 
 beforeAll(() => {
-  fse.emptyDirSync(TEMP_CWD);
+  fse.emptyDirSync(TEMP);
 });
 
 afterEach(() => {
+  fse.emptyDirSync(TEMP);
   jest.clearAllMocks();
 });
 
 afterAll(() => {
-  fse.removeSync(TEMP_CWD);
+  fse.removeSync(TEMP);
 });
 
 describe('cli', () => {
-  test('should print help info without any option or command', async () => {
-    const { stdout, stderr } = await bin(null)('');
-    expect.assertions(5);
-    expect(spyProgramHelp).toBeCalled();
-    expect(stdout).toMatch(/Examples\:/);
-    expect(stdout).toMatch(/smash init/);
-    expect(stdout).toMatch(/smash install smash-template-react/);
-    expect(stdout).toMatch(/smash run helloworld/);
+  describe('print help info', () => {
+    it('should print help info without any option or command', async () => {
+      const { stdout, stderr } = await bin(null)('');
+      expect.assertions(5);
+      expect(spyProgramHelp).toBeCalled();
+      expect(stdout).toMatch(/Examples\:/);
+      expect(stdout).toMatch(/smash init/);
+      expect(stdout).toMatch(/smash install smash-template-react/);
+      expect(stdout).toMatch(/smash run helloworld/);
+    });
+
+    it('should print help info with "--help" option', async () => {
+      const { stdout, stderr } = await bin(null)('--help');
+      expect.assertions(5);
+      expect(spyProgramHelp).toBeCalled();
+      expect(stdout).toMatch(/Examples\:/);
+      expect(stdout).toMatch(/smash init/);
+      expect(stdout).toMatch(/smash install smash-template-react/);
+      expect(stdout).toMatch(/smash run helloworld/);
+    });
+
+    it('should print help info with "-h" option', async () => {
+      const { stdout, stderr } = await bin(null)('-h');
+      expect.assertions(5);
+      expect(spyProgramHelp).toBeCalled();
+      expect(stdout).toMatch(/Examples\:/);
+      expect(stdout).toMatch(/smash init/);
+      expect(stdout).toMatch(/smash install smash-template-react/);
+      expect(stdout).toMatch(/smash run helloworld/);
+    });
   });
 
-  test('should print help info with "-h|--help" option', async () => {
-    const { stdout, stderr } = await bin(null)('--help');
-    expect.assertions(5);
-    expect(spyProgramHelp).toBeCalled();
-    expect(stdout).toMatch(/Examples\:/);
-    expect(stdout).toMatch(/smash init/);
-    expect(stdout).toMatch(/smash install smash-template-react/);
-    expect(stdout).toMatch(/smash run helloworld/);
-  });
-
-  test('shold run smash-init with "init" command', async () => {
-    const { stdout, stderr } = await bin('init');
+  it('shold run smash-init with "init" command', async () => {
+    const { stdout, stderr } = await bin(TEMP)('init');
 
     expect(spySmashInit).toBeCalled();
   });
 
-  test('shold run smash-install with "install|i" command', async () => {
-    const { stdout, stderr } = await bin('install smash-template-react');
+  describe('install|i', () => {
+    it('shold run smash-install with "install" command', async () => {
+      const { stdout, stderr } = await bin(TEMP)('install smash-template-react');
+      expect(spySmashInstall).toBeCalled();
+    });
 
-    expect(spySmashInstall).toBeCalled();
+    it('shold run smash-install with "i" command', async () => {
+      const { stdout, stderr } = await bin(TEMP)('i smash-template-react');
+      expect(spySmashInstall).toBeCalled();
+    });
   });
 
-  test('shold run smash-run with "run|r" command', async () => {
-    const { stdout, stderr } = await bin('run helloworld');
+  describe('run|r', () => {
+    const cwd = resolve(ROOT, '__fixtures__/smash-project');
+    it('shold run smash-run with "run" command', async () => {
+      const { stdout, stderr } = await bin(cwd)('run helloworld');
+      expect(spySmashRun).toBeCalled();
+    });
 
-    expect(spySmashRun).toBeCalled();
+    it('shold run smash-run with "r" command', async () => {
+      const { stdout, stderr } = await bin(cwd)('r helloworld');
+      expect(spySmashRun).toBeCalled();
+    });
   });
 });
