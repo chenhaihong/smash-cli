46ce2f5d2705a319d6cbe17d6ee93984e9730eb3
erye <erye@tiiit.cn>
2019-08-08 20:28:05 +0800

    1.添加清空仓库setup

--- scripts/setup-empty-repo.js
+++ scripts/setup-empty-repo.js
@@ -6,10 +6,4 @@ const HOME = os.homedir();
 const REPO = resolve(HOME, '.smash-cli'); // 存放中间件与模板的仓库
 
 // 清空仓库
-beforeAll(() => {
-  fse.emptyDirSync(REPO);
-});
-
-afterAll(() => {
-  fse.emptyDirSync(REPO);
-});
+fse.emptyDirSync(REPO);
