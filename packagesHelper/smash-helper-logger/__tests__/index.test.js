b70b5691ac910084a9396a66983b4f507640e29c
erye <erye@tiiit.cn>
2019-08-09 10:47:38 +0800

    1.更新smash-helper-logger，支持自定义name

--- packagesHelper/smash-helper-logger/__tests__/index.test.js
+++ packagesHelper/smash-helper-logger/__tests__/index.test.js
@@ -4,11 +4,8 @@ const chalk = require('chalk');
 const Logger = require('../lib');
 
 jest.spyOn(console, 'log');
-jest.mock('../lib');
 
 afterEach(() => {
-  // Clear all instances and calls to constructor and all methods:
-  Logger.mockClear();
   jest.clearAllMocks();
 });
 
@@ -23,69 +20,25 @@ describe('smash-helper-logger', () => {
     });
   });
 
-  it('logger.log should print well', () => {
+  it('should create a named-smash logger instance well without param name', function() {
+    expect(new Logger().name).toBe('smash');
+  });
+
+  it('logger[method] should print well', () => {
     const _logger = new Logger('smash');
     _logger.log('Hello World');
+    _logger.info('Hello World');
+    _logger.warn('Hello World');
+    _logger.error('Hello World');
+    _logger.success('Hello World');
+    _logger.fail('Hello World');
 
-    expect(Logger).toHaveBeenCalled();
-
-    const mockLoggerInstance = Logger.mock.instances[0];
-    const mockLog = mockLoggerInstance.log;
-    expect(mockLog).toHaveBeenCalled();
-    expect(mockLog.mock.calls[0][0]).toEqual('Hello World');
-
-    // expect(console.log).toHaveBeenCalled();
-
-    // expect(console.log.mock.calls[0]).toEqual(['smash', chalk.blueBright('log'), 'Hello World']);
-  });
-
-  it('logger.info should print well', () => {
-    new Logger('smash').info('Hello World');
-
-    const mockInfo = Logger.mock.instances[0].info;
-    expect(mockInfo).toHaveBeenCalled();
-    expect(mockInfo.mock.calls[0][0]).toEqual('Hello World');
-
-    // expect(console.log.mock.calls[0]).toEqual(['smash', chalk.greenBright('info'), 'Hello World']);
-  });
-
-  it('logger.warn should print well', () => {
-    new Logger('smash').warn('Hello World');
-
-    const mockWarn = Logger.mock.instances[0].warn;
-    expect(mockWarn).toHaveBeenCalled();
-    expect(mockWarn.mock.calls[0][0]).toEqual('Hello World');
-
-    // expect(console.log.mock.calls[0]).toEqual(['smash', chalk.yellowBright('warn'), 'Hello World']);
-  });
-
-  it('logger.error should print well', () => {
-    new Logger('smash').error('Hello World');
-
-    const mockError = Logger.mock.instances[0].error;
-    expect(mockError).toHaveBeenCalled();
-    expect(mockError.mock.calls[0][0]).toEqual('Hello World');
-
-    // expect(console.log.mock.calls[0]).toEqual(['smash', chalk.redBright('error'), 'Hello World']);
-  });
-
-  it('logger.success should print well', () => {
-    new Logger('smash').success('Hello World');
-
-    const mockSuccess = Logger.mock.instances[0].success;
-    expect(mockSuccess).toHaveBeenCalled();
-    expect(mockSuccess.mock.calls[0][0]).toEqual('Hello World');
-
-    // expect(console.log.mock.calls[0]).toEqual(['smash', chalk.greenBright('success'), 'Hello World']);
-  });
-
-  it('logger.fail should print well', () => {
-    new Logger('smash').fail('Hello World');
-
-    const mockFail = Logger.mock.instances[0].fail;
-    expect(mockFail).toHaveBeenCalled();
-    expect(mockFail.mock.calls[0][0]).toEqual('Hello World');
-
-    // expect(console.log.mock.calls[0]).toEqual(['smash', chalk.redBright('fail'), 'Hello World']);
+    expect(console.log.mock.calls.length).toBe(6);
+    expect(console.log.mock.calls[0]).toEqual(['smash', chalk.blueBright('log'), 'Hello World']);
+    expect(console.log.mock.calls[1]).toEqual(['smash', chalk.greenBright('info'), 'Hello World']);
+    expect(console.log.mock.calls[2]).toEqual(['smash', chalk.yellowBright('warn'), 'Hello World']);
+    expect(console.log.mock.calls[3]).toEqual(['smash', chalk.redBright('error'), 'Hello World']);
+    expect(console.log.mock.calls[4]).toEqual(['smash', chalk.greenBright('success'), 'Hello World']);
+    expect(console.log.mock.calls[5]).toEqual(['smash', chalk.redBright('fail'), 'Hello World']);
   });
 });
