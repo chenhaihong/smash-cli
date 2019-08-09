50e5dc5d812c17e24524af51e986cbcb1ffecce8
erye <erye@tiiit.cn>
2019-08-09 18:09:01 +0800

    1.中间件使用另外一个测试脚本入口

--- packagesMiddleware/smash-middleware-clean/lib/index.js
+++ packagesMiddleware/smash-middleware-clean/lib/index.js
@@ -2,10 +2,18 @@
  * A directory cleaner middleware for smash-cli.
  */
 
-const path = require('path');
+const { join } = require('path');
 const fs = require('fs-extra');
 
-module.exports = function middleware(ctx, config, next) {
+module.exports = SmashMiddlewareClean;
+
+/**
+ * Directory cleaner.
+ * @param {Object} ctx
+ * @param {Object} config 配置对象
+ * @param {Function|null} next 下一个待执行的中间件函数
+ */
+function SmashMiddlewareClean(ctx, config, next) {
   // （1）获取并规范化参数
   let { dirs, remove = false } = config;
   typeof dirs === 'string' && (dirs = [dirs]);
@@ -16,11 +24,8 @@ module.exports = function middleware(ctx, config, next) {
     // 目录是空，进入下一个
     if (!dir) return;
 
-    // 如果是 / 或 \ 开头的目录，加上 . 前缀，避免对根目录进行清空操作
-    /^(\/|\\)/.test(dir) && (dir = '.' + dir);
-
     // 目录不存在，直接进入下一个
-    dir = path.resolve(process.cwd(), dir);
+    dir = join(process.cwd(), dir);
     if (fs.pathExistsSync(dir)) {
       const method = remove ? 'removeSync' : 'emptyDirSync';
       fs[method](dir);
@@ -28,4 +33,4 @@ module.exports = function middleware(ctx, config, next) {
   });
 
   next && next();
-};
+}
--- scripts/test-middleware.js
+++ scripts/test-middleware.js
@@ -5,19 +5,21 @@ const minimist = require('minimist');
 const argv = minimist(process.argv.slice(2));
 
 let rootDir = resolve(__dirname, '../');
-if (argv.package) {
-  // 指定包测试
-  rootDir = join(rootDir, argv.package);
-  console.log('testing:', rootDir);
-  if (!fse.pathExistsSync(rootDir)) {
-    return console.log('路径不存在:', rootDir);
-  }
+if (!argv.package) {
+  return console.log('必须指定包测试');
 }
+
+rootDir = join(rootDir, argv.package);
+if (!fse.pathExistsSync(rootDir)) {
+  return console.log('路径不存在:', rootDir);
+}
+
+console.log('testing:', rootDir);
 
 const args = [
   '--runInBand', //顺序执行
   '--config',
-  resolve(__dirname, '../jest.config.main.js'),
+  resolve(__dirname, '../jest.config.middleware.js'),
   `--rootDir`,
   rootDir,
 ];
