a4a385c83c540aeefe523e1d42a26f2066a29d8e
erye <erye@tiiit.cn>
2019-08-13 10:41:45 +0800

    1.smash-middleware-helloworld，更新测试用例

--- packagesMiddleware/smash-middleware-helloworld/__tests__/index.test.js
+++ packagesMiddleware/smash-middleware-helloworld/__tests__/index.test.js
@@ -4,12 +4,12 @@ const ctx = {};
 const config = {};
 const next = () => {};
 
-const spyLog = jest.spyOn(console, log);
+const spyLog = jest.spyOn(console, 'log');
 const mockNext = jest.fn(next);
 
 describe('SmashHelloWorld', () => {
   it('should run well', () => {
-    SmashHelloWorld(ctx, config, next);
+    SmashHelloWorld(ctx, config, mockNext);
 
     expect(spyLog).toBeCalled();
     expect(spyLog.mock.calls[0][0]).toBe('Hello world');
