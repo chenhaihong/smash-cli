191d1e3b0c82e7a805c1fcad532f4650585ea7e1
erye <erye@tiiit.cn>
2019-08-24 01:43:35 +0800

    1.更新文档footer链接

--- docusaurus/website/core/Footer.js
+++ docusaurus/website/core/Footer.js
@@ -9,12 +9,12 @@ const React = require('react');
 
 class Footer extends React.Component {
   docUrl(doc, language) {
-    console.log(doc, language);
     const baseUrl = this.props.config.baseUrl;
     const docsUrl = this.props.config.docsUrl;
     const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
     const langPart = `${language ? `${language}/` : ''}`;
-    return `${baseUrl}${docsPart}${langPart}${doc}`;
+    // return `${baseUrl}${docsPart}${langPart}${doc}`;
+    return `${baseUrl}${docsPart}${doc}`;
   }
 
   pageUrl(doc, language) {
