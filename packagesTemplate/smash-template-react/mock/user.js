04187ab10ab65217e0b2a2dd083cb175a296a29b
erye <erye@tiiit.cn>
2019-09-26 23:47:27 +0800

    :bento: Update smash-template-react.

--- packagesTemplate/smash-template-react/mock/user.js
+++ packagesTemplate/smash-template-react/mock/user.js
@@ -1,3 +1,11 @@
+function sleep(delay) {
+  return new Promise(function(resolve) {
+    setTimeout(() => {
+      resolve(1);
+    }, delay);
+  });
+}
+
 module.exports = {
   '/user/profile': {
     // 请求类型
@@ -10,7 +18,8 @@ module.exports = {
   '/user/login': {
     method: 'post',
     // 支持纯函数类型，返回结果为纯函数执行结果
-    result() {
+    async result(req, res, next) {
+      await sleep(300);
       return {
         success: Math.random() > 0.5,
       };
