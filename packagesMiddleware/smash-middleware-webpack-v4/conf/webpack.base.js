a2faf23de1417e9aef165bdbb62b5b141d8243f2
erye <erye@tiiit.cn>
2019-10-07 01:24:32 +0800

    :heavy_plus_sign: :heavy_minus_sign: Update smash-middleware-webpack-v4

--- packagesMiddleware/smash-middleware-webpack-v4/conf/webpack.base.js
+++ packagesMiddleware/smash-middleware-webpack-v4/conf/webpack.base.js
@@ -76,11 +76,7 @@ module.exports = function() {
             loader: 'babel-loader',
             options: {
               cacheDirectory: true,
-              presets: [
-                require.resolve('@babel/preset-env'),
-                require.resolve('@babel/preset-react'),
-              ],
-              plugins: getAllStagePluginsOfBabel(),
+              presets: [require.resolve('smash-helper-babel-preset-react')],
             },
           },
         },
@@ -113,57 +109,3 @@ module.exports = function() {
     },
   };
 };
-
-/**
- * 获取babel stage0、1、2、3的所有插件
- *
- * As of Babel v7, all the stage presets have been deprecated. Check the blog post for more information.
- * https://babeljs.io/blog/2018/07/27/removing-babels-stage-presets
- *
- * For upgrade instructions, see the README.
- * https://github.com/babel/babel/blob/master/packages/babel-preset-stage-0/README.md
- */
-function getAllStagePluginsOfBabel() {
-  return [
-    // Stage 0
-    require.resolve('@babel/plugin-proposal-function-bind'),
-
-    // Stage 1
-    require.resolve('@babel/plugin-proposal-export-default-from'),
-    require.resolve('@babel/plugin-proposal-logical-assignment-operators'),
-    [
-      require.resolve('@babel/plugin-proposal-optional-chaining'),
-      { loose: false },
-    ],
-    [
-      require.resolve('@babel/plugin-proposal-pipeline-operator'),
-      { proposal: 'minimal' },
-    ],
-    [
-      require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'),
-      { loose: false },
-    ],
-    require.resolve('@babel/plugin-proposal-do-expressions'),
-
-    // Stage 2
-    [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
-    require.resolve('@babel/plugin-proposal-function-sent'),
-    require.resolve('@babel/plugin-proposal-export-namespace-from'),
-    require.resolve('@babel/plugin-proposal-numeric-separator'),
-    require.resolve('@babel/plugin-proposal-throw-expressions'),
-
-    // Stage 3
-    require.resolve('@babel/plugin-syntax-dynamic-import'),
-    require.resolve('@babel/plugin-syntax-import-meta'),
-    [
-      require.resolve('@babel/plugin-proposal-class-properties'),
-      { loose: true },
-    ],
-    require.resolve('@babel/plugin-proposal-json-strings'),
-
-    // A plugin that enables the re-use of Babel's injected helper code to save on codesize.
-    // 为什么会出现这个插件，以及为什么使用它？
-    // 阅读：https://babeljs.io/docs/en/babel-plugin-transform-runtime#why
-    require.resolve('@babel/plugin-transform-runtime'),
-  ];
-}
