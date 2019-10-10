ff47d334faffaa3ee70bdcd2ab684c0a274f1510
erye <erye@tiiit.cn>
2019-10-10 16:20:40 +0800

    :white_check_mark: :construction_worker: Update tests and jest config.
    1. :white_check_mark: smash-helper-babel-preset-react, update tests.
    2. :construction_worker: Update jest.config.main.js

--- jest.config.main.js
+++ jest.config.main.js
@@ -18,7 +18,7 @@ module.exports = {
   coveragePathIgnorePatterns: [
     // 忽略覆盖
     '/node_modules/',
-    '/packagesHelper/',
+    // '/packagesHelper/',
     '/packagesMiddleware/',
     '/packagesTemplate/',
   ],
@@ -29,7 +29,7 @@ module.exports = {
   testPathIgnorePatterns: [
     // 忽略测试路径
     '/node_modules/',
-    '/packagesHelper/',
+    // '/packagesHelper/',
     '/packagesMiddleware/',
     '/packagesTemplate/',
   ],
--- packagesHelper/smash-helper-babel-preset-react/__tests__/index.test.js
+++ packagesHelper/smash-helper-babel-preset-react/__tests__/index.test.js
@@ -1,14 +1,32 @@
 'use strict';
+const preset = require('../lib');
 
-const { transform } = require('@babel/core');
-const presetUrl = require.resolve('../lib');
-const presets = [presetUrl];
-// TODO 完成测试用例
-test('@babel/plugin-syntax-dynamic-import', () => {
-  const { code } = transform(`import('./a');`, { presets });
-  const expected = `
-"use strict";
-import('./a');
-    `.trim();
-  expect(code).toEqual(expected);
+if (!process.env) {
+  process.env = {};
+}
+
+// 偷懒型测试，只验证了输出必须包含的字段和长度。
+// 没有严格测试各个提案的输入和输出是否正确。
+describe('smash-helper-babel-preset-react', function() {
+  test('with NODE_ENV=production', () => {
+    process.env.NODE_ENV = 'production';
+
+    const opts = preset();
+    expect(opts).toHaveProperty('plugins');
+    expect(opts).toHaveProperty('presets');
+
+    expect(opts.plugins.length).toEqual(18);
+    expect(opts.presets.length).toEqual(2);
+  });
+
+  test('without NODE_ENV=production', () => {
+    process.env.NODE_ENV = null;
+
+    const opts = preset();
+    expect(opts).toHaveProperty('plugins');
+    expect(opts).toHaveProperty('presets');
+
+    expect(opts.plugins.length).toEqual(17);
+    expect(opts.presets.length).toEqual(2);
+  });
 });
