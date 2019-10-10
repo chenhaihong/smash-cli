2f0659fb7b1325a3de2808d370292cc689650643
erye <erye@tiiit.cn>
2019-10-10 15:15:14 +0800

    :package: Add empty smash-helper-babel-preset-env

---
+++ packagesHelper/smash-helper-babel-preset-env/__tests__/index.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const smashHelperBabelPresetEnv = require('../lib');
+
+describe('smash-helper-babel-preset-env', () => {
+  it('needs tests');
+});
---
+++ packagesHelper/smash-helper-babel-preset-env/lib/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = smashHelperBabelPresetEnv;
+
+function smashHelperBabelPresetEnv() {
+  // TODO
+}
