3e5f24df3d3518f1f202a9e5e009d2b6fe5bedf4
erye <erye@tiiit.cn>
2019-10-10 16:26:44 +0800

    :white_check_mark: Add empty tests for smash-helper-babel-preset-env

--- packagesHelper/smash-helper-babel-preset-env/__tests__/index.test.js
+++ packagesHelper/smash-helper-babel-preset-env/__tests__/index.test.js
@@ -3,5 +3,8 @@
 const smashHelperBabelPresetEnv = require('../lib');
 
 describe('smash-helper-babel-preset-env', () => {
-  it('needs tests');
+  it('needs tests', function() {
+    smashHelperBabelPresetEnv();
+    expect(1).toBe(1);
+  });
 });
