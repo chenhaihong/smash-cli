6a6b63208c5b55be41c74984b67f9769dad3d86e
erye <erye@tiiit.cn>
2019-10-26 18:07:41 +0800

    :heavy_plus_sign: :package: Update webpack-v4，add vue@2
    1. webpack-v4，add node-sass sass-loader
    2. add private template vue@2

---
+++ packagesMiddleware/smash-middleware-webpack-v4/__fixtures__/vueV2/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello-vue-v2',
+  data() {
+    return {
+      title: 'Hello vue@2',
+    };
+  },
+};
+</script>
---
+++ packagesTemplate/smash-template-vue-v2/README.md
@@ -0,0 +1,11 @@
+# `smash-template-vue-v2`
+
+> TODO: description
+
+## Usage
+
+```
+const smashTemplateVueV2 = require('smash-template-vue-v2');
+
+// TODO: DEMONSTRATE API
+```
