229aa304dfddfbc874060650cd5fe235bd863ba2
erye <erye@tiiit.cn>
2020-02-01 23:40:39 +0800

    :white_check_mark: Add tests for smash-cli

--- RepoMain/packagesMain/smash-cli/__tests__/smash.test.js
+++ RepoMain/packagesMain/smash-cli/__tests__/smash.test.js
@@ -1,13 +1,15 @@
+const os = require('os');
 const { resolve } = require('path');
 const execShPromise = require('exec-sh').promise;
 const fse = require('fs-extra');
 
+const REPO_MIDDLEWARE = resolve(os.homedir(), '.smash-cli', 'middleware');
 const ROOT = resolve(__dirname, '..'); // 该包的根目录
 const TEMP = resolve(ROOT, '__temp__');
 const smashBin = resolve(ROOT, 'lib/smash');
 const bin = (cwd) => (command) => execShPromise(`node ${smashBin} ${command}`, { cwd, stdio: null });
 
-jest.setTimeout(100e3);
+jest.setTimeout(5000e3);
 
 beforeAll(() => {
   fse.emptyDirSync(TEMP);
@@ -22,14 +24,15 @@ afterAll(() => {
   fse.removeSync(TEMP);
 });
 
-describe('cli', () => {
+describe('smash', () => {
   // 测试帮助命令
   describe('should print help info', () => {
     it('without any option or command', async (done) => {
       const { stdout, stderr } = await bin(null)('');
       expect(stdout).toMatch(/Examples\:/);
       expect(stdout).toMatch(/smash init/);
-      expect(stdout).toMatch(/smash install smash-template-react/);
+      expect(stdout).toMatch(/smash download smash-template-react/);
+      expect(stdout).toMatch(/smash install/);
       expect(stdout).toMatch(/smash run helloworld/);
 
       done();
@@ -39,7 +42,8 @@ describe('cli', () => {
       const { stdout, stderr } = await bin(null)('--help');
       expect(stdout).toMatch(/Examples\:/);
       expect(stdout).toMatch(/smash init/);
-      expect(stdout).toMatch(/smash install smash-template-react/);
+      expect(stdout).toMatch(/smash download smash-template-react/);
+      expect(stdout).toMatch(/smash install/);
       expect(stdout).toMatch(/smash run helloworld/);
 
       done();
@@ -49,7 +53,8 @@ describe('cli', () => {
       const { stdout, stderr } = await bin(null)('-h');
       expect(stdout).toMatch(/Examples\:/);
       expect(stdout).toMatch(/smash init/);
-      expect(stdout).toMatch(/smash install smash-template-react/);
+      expect(stdout).toMatch(/smash download smash-template-react/);
+      expect(stdout).toMatch(/smash install/);
       expect(stdout).toMatch(/smash run helloworld/);
 
       done();
@@ -60,7 +65,7 @@ describe('cli', () => {
   describe('init', () => {
     const cwd = resolve(TEMP, 'dir_for_init');
 
-    it('should initialize non-smash directory well', async (done) => {
+    it('should initialize un-initialized directory well', async (done) => {
       fse.ensureDirSync(cwd);
       const { stdout, stderr } = await bin(cwd)('init');
       expect(stdout).toMatch(/Initialized successfully\./);
@@ -76,22 +81,48 @@ describe('cli', () => {
     });
   });
 
-  // TODO 测试安装中间件命令
-  describe('install', () => {
-    it('should run smash-install with "install" command', async (done) => {
-      const cwd = resolve(TEMP, 'dir_for_install');
+  // 测试下载模板包命令
+  describe('download', () => {
+    it('should run smash-download with "download" command', async (done) => {
+      const cwd = resolve(TEMP, 'dir_for_download');
       fse.ensureDirSync(cwd);
-      const { stdout, stderr } = await bin(cwd)('install smash-template-react');
-      expect(stdout).toMatch(/Successfully installed/);
+      const { stdout, stderr } = await bin(cwd)('download smash-template-react');
+      expect(stdout).toMatch(/Successfully downloaded/);
 
       done();
     });
 
-    it('should run smash-install with "i" command', async (done) => {
-      const cwd = resolve(TEMP, 'dir_for_i');
+    it('should run smash-download with "d" command', async (done) => {
+      const cwd = resolve(TEMP, 'dir_for_d');
       fse.ensureDirSync(cwd);
-      const { stdout, stderr } = await bin(cwd)('i smash-template-react');
-      expect(stdout).toMatch(/Successfully installed/);
+      const { stdout, stderr } = await bin(cwd)('d smash-template-react');
+      expect(stdout).toMatch(/Successfully downloaded/);
+
+      done();
+    });
+  });
+
+  // 测试安装中间件命令
+  describe('install', () => {
+    // 清空中间件仓库
+    fse.emptyDirSync(REPO_MIDDLEWARE);
+    const cwd = resolve(ROOT, '__fixtures__/smash-project');
+
+    // 首次安装
+    it('should run smash-install with "install" command', async (done) => {
+      fse.ensureDirSync(cwd);
+      const { stdout, stderr } = await bin(cwd)('install');
+      expect(stdout).toMatch(/Installing\.\.\./);
+      expect(stdout).toMatch(/Successfully installed\./);
+
+      done();
+    });
+
+    // 二次安装
+    it('should run smash-install with "i" command', async (done) => {
+      fse.ensureDirSync(cwd);
+      const { stdout, stderr } = await bin(cwd)('i');
+      expect(stdout).toMatch(/Already installed\./);
 
       done();
     });
@@ -102,6 +133,9 @@ describe('cli', () => {
     const cwd = resolve(ROOT, '__fixtures__/smash-project');
 
     it('should run smash-run with "run" command', async (done) => {
+      // 需要先安装好中间件包
+      await bin(cwd)('install');
+
       const { stdout, stderr } = await bin(cwd)('run helloworld');
       expect(stdout).toMatch(/Hello world/);
 
@@ -109,6 +143,9 @@ describe('cli', () => {
     });
 
     it('should run smash-run with "r" command', async (done) => {
+      // 需要先安装好中间件包
+      await bin(cwd)('install');
+
       const { stdout, stderr } = await bin(cwd)('r helloworld');
       expect(stdout).toMatch(/Hello world/);
 
